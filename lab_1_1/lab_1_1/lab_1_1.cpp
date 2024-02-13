870a845d2ac33707bafd2d9645f00b75fb5b7a5c
aleks <aleks@RUSLAN>
2024-02-13 22:23:22 +0500

    Добавьте файлы проекта.

---
+++ lab_1_1/lab_1_1/lab_1_1.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int i, k,num;
+	std::cout << "Input number: ";
+	std::cin >> num;
+	int power(int, int);
+
+
+
+	for (i = 1; i < 10; i++) {
+		k = power(num, i);
+		cout << "i = " << i << ", k = " << k << endl;
+	}
+}
+
+int power(int x, int n) {
+	int i,p=1;
+	for (i = 1; i <= n; i++) {
+		p *= x;
+	}
+	return p;
+}
---
+++ lab_1_3/lab_1_3/lab_1_3.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+void main(void) {
+	int a = 2, b = 5;
+	void obmen1(int, int);
+	void obmen1(int*, int*);
+	void obmen1(int&, int&);
+
+	std::cout << "not obmen: a = " <<a<< " b = " <<b;
+	// вызов obmen1;
+	std::cout << " obmen_1: a = " << a << " b = " << b;
+	// вызов obmen2;
+	std::cout << " obmen_2: a = " << a << " b = " << b;
+	// вызов obmen3;
+	std::cout << " obmen_3: a = " << a << " b = " << b;
+}
